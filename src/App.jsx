adca3f5552d1c9ef4c58a94a29dcf72234543953
MazaFaaka <haitovvasyl@gmail.com>
2025-04-21 13:58:58 +0200

    Homework

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,8 @@
-import Profile from './components/Profile/Profile';
-import userData from './data/userData.json';
-import FriendList from './components/FriendList/Friendlist';
-import friends from './data/friends.json';
-import TransactionHistory from './components/TransactionHistory/TransactionHistory';
-import transactions from './data/transactions.json';
 
 export default function App() {
   return (
     <>
-      <Profile
-        name={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        image={userData.avatar}
-        stats={userData.stats}
-      />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
+   Hello
     </>
   );
 }
